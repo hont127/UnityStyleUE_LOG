7ca1e75e4a3f4e03851549ecfa281b415ff446ee
hont <hont127@163.com>
2022-05-18 18:13:33 +0800

    Update Debug.h

--- Debug.h
+++ Debug.h
@@ -8,9 +8,13 @@
 #define Debug_Warning_TEXT(format, ...) Debug_Internal(Warning, TEXT(format),##__VA_ARGS__)
 #define Debug_Error_TEXT(format, ...) Debug_Internal(Error, TEXT(format),##__VA_ARGS__)
 
-#define Debug_Log(format, ...) Debug_Internal(Log, format,##__VA_ARGS__)
-#define Debug_Warning(format, ...) Debug_Internal(Warning, format,##__VA_ARGS__)
-#define Debug_Error(format, ...) Debug_Internal(Error, format,##__VA_ARGS__)
+//#define Debug_Log(format, ...) Debug_Internal(Log, format,##__VA_ARGS__)
+//#define Debug_Warning(format, ...) Debug_Internal(Warning, format,##__VA_ARGS__)
+//#define Debug_Error(format, ...) Debug_Internal(Error, format,##__VA_ARGS__)
+
+#define Debug_Log(content) Debug_Internal(Log, TEXT("%s"),content)
+#define Debug_Warning(content) Debug_Internal(Warning, TEXT("%s"),content)
+#define Debug_Error(content) Debug_Internal(Error, TEXT("%s"),content)
 
 #define Debug_Internal(logType, format, ...) UE_LOG(LogTemp, ##logType, TEXT("%s \n (%s Line:%d Func:%s)"), *Debug_Internal_(format,##__VA_ARGS__), TEXT(__FILE__), __LINE__, TEXT(__FUNCTION__))
 #define Debug_Internal_(format, ...) FString::Printf(format,##__VA_ARGS__)
